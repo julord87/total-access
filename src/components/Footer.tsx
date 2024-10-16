a1e09315c63c699a6dbe3b9840a5096e68a6b04b
julord87 <julianmzg@gmail.com>
2024-10-16 12:04:36 -0300

    Refactor Footer component to center-align text and dynamically display current year in copyright message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="mt-4 text-xs lg:text-sm font-color opacity-45 pl-6 lg:pl-12 pb-5">
+    <footer className=" text-centermt-4 text-xs lg:text-sm font-color opacity-45 pl-6 lg:pl-12 pb-5">
       <p>
-        &copy; 2024 Total Access n Logistics. Todos los derechos reservados.
+        {`© ${new Date().getFullYear()} Total Access n Logistics. Todos los derechos reservados.`}
       </p>
     </footer>
   );
