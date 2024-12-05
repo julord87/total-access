e3379c47a1ac364609de5e74bae83809265bc80f
julord87 <julianmzg@gmail.com>
2024-12-05 12:39:21 -0300

    Fix formatting issue in navigation class for improved layout consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,7 @@ const Header: React.FC<HeaderProps> = ({ language, setLanguage }) => {
       <nav
         className={`lg:flex lg:space-x-2 xl:space-x-8 font-color ${
           isOpen ? "block justify-items-end" : "hidden"
-        } nav lg:block absolute lg:staticrounded-md top-16 right-4 lg:w-full md:w-auto lg:bg-transparent p-4 lg:p-0 z-20`}
+        } nav lg:block absolute lg:static rounded-md top-16 right-4 lg:w-full md:w-auto lg:bg-transparent p-4 lg:p-0 z-20`}
       >
         <Link href="/nuestra_firma" className="block py-2 lg:inline hover:opacity-75 text-sm">
           {language === "EN" ? "Our Firm" : language === "ZH" ? "我们的公司" : "Nuestra Firma"}
